37596ae6eb11b9c4f0a4320715d4edd934896f96
Steven Avon <savon40@gmail.com>
2019-02-17 15:40:08 -0600

    Add files via upload

---
+++ httpclient.py
@@ -0,0 +1,23 @@
+import requests
+
+class HttpClient(object):
+    def get(self, url, **kwargs):
+        response = requests.get(url, **kwargs)
+
+        return self._response(response)
+
+    def post(self, url, data = None, **kwargs):
+        response = requests.post(url, data, **kwargs)
+
+        return self._response(response)
+
+    def _response(self, r):
+        if r.status_code == 200:
+            return HttpClientResponse(True, r.content)
+        return HttpClientResponse()
+
+
+class HttpClientResponse(object):
+    def __init__(self, success = False, content = ''):
+        self.success = success
+        self.content = content
